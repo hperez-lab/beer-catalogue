d74f4ac952b1e0deb0a08c078c31e45117f16a86
Hector Perez <hectorperez70@gmail.com>
2021-04-16 18:18:23 +0200

    Initial commit

---
+++ beer-catalogue/src/test/java/com/beer/catalogue/beercatalogue/BeerCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beer.catalogue.beercatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
