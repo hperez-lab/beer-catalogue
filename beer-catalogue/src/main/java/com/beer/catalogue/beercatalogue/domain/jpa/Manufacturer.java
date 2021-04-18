2b9273bcbc9b3ca529626e82658a93b74b6e1958
Hector Perez <hectorperez70@gmail.com>
2021-04-18 18:38:44 +0200

    Add hashCode and equals to entities

--- beer-catalogue/src/main/java/com/beer/catalogue/beercatalogue/domain/jpa/Manufacturer.java
+++ beer-catalogue/src/main/java/com/beer/catalogue/beercatalogue/domain/jpa/Manufacturer.java
@@ -35,4 +35,29 @@ public class Manufacturer {
 	private Set<Beer> beers = new HashSet<>();
 	@OneToOne(cascade = CascadeType.ALL)
 	private User user;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Manufacturer other = (Manufacturer) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
